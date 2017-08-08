2355d4da4039896a8477e3567cad282cf9f4d0cc
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2017-08-08 11:41:49 +0300

    mock response time added

--- src/testing/mock-backend.service.ts
+++ src/testing/mock-backend.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpResponse, HttpRequest } from "@angular/common/http";
 import { HttpTestingController, RequestMatch, TestRequest } from "@angular/common/http/testing";
-import { defer } from "lodash";
 
 import { Requester } from "../requester";
 import { Inheritor } from "../helpers";
@@ -17,16 +16,17 @@ export class MockBackendService {
 
 	public addResponse(
 		match: string | RequestMatch | ((req: HttpRequest<any>) => boolean),
-		response: ArrayBuffer | Blob | string | number | Object | (string | number | Object | null)[] | null
+		response: ArrayBuffer | Blob | string | number | Object | (string | number | Object | null)[] | null,
+		waitFor = 0
 	): void {
 		const map = new Map();
 		map.set(RequestFiredEvent, [(ev: RequestFiredEvent<any>) => {
 			this.testingController.match(match).forEach(req => {
-				defer(() => {
+				setTimeout(() => {
 					if (!req.cancelled) {
 						req.flush(response);
 					}
-				});
+				}, waitFor);
 			});
 		}]);
 
